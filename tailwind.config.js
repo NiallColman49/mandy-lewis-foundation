4bb694f4da4a61e744099d3faeac2a6b359b443e
Niall <hello@niallcolman.co.uk>
2024-01-14 12:30:19 +0000

    added theme fonts * set default styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
     content: ["./*.html"],
     theme: {
+        container: {
+            DEFAULT: "1rem",
+            sm: "2rem",
+            lg: "4rem",
+            xl: "5rem",
+            "2xl": "6rem",
+            center: true,
+        },
         extend: {},
     },
     plugins: [],
