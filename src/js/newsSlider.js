99acf051127e51110f7e005a78b62e8df41f5a49
Niall <hello@niallcolman.co.uk>
2024-01-16 19:08:15 +0000

    WIP

--- src/js/newsSlider.js
+++ src/js/newsSlider.js
@@ -1,19 +1,22 @@
 $(".news-slider").slick({
-    dots: true,
+    dots: false,
+    arrows: true,
+    prevArrow: $(".prev-slide"),
+    nextArrow: $(".next-slide"),
     infinite: false,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
     responsive: [
         {
-            breakpoint: 600,
+            breakpoint: 1024,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2,
             },
         },
         {
-            breakpoint: 480,
+            breakpoint: 600,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
