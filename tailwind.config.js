ea66b222c12f7c2a7d6b07fb1d57d6ad1a53a363
Niall <hello@niallcolman.co.uk>
2024-02-08 21:23:07 +0000

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,11 @@ module.exports = {
             },
             colors: {
                 "mlf-pink": "#e35e9d",
-                "mlf-purple": "#141740",
+                // "mlf-purple": "#141740",
                 "mlf-dark-purple": "#51174d",
                 "mlf-light-pink": "#efb2ce",
                 "mlf-bright-pink": "#FF398B",
+                "mlf-purple": "#FFAED7",
             },
         },
     },
